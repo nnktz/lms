8c092fe89d31627599bc5d2177df7b3f551aa1ff
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-10-24 11:39:15 +0700

    Course category form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -7,6 +7,7 @@ import { IconBadge } from '@/components/icon-badge';
 import { TitleForm } from './_components/title-form';
 import { DescriptionForm } from './_components/description-form';
 import { ImageForm } from './_components/image-form';
+import { CategoryForm } from './_components/category-form';
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -23,6 +24,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   if (!course) {
     return redirect('/');
   }
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: 'asc',
+    },
+  });
 
   const requiredFields = [
     course.title,
@@ -69,6 +76,15 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           <ImageForm
             initialData={course}
             courseId={course.id}
+          />
+
+          <CategoryForm
+            initialData={course}
+            courseId={course.id}
+            options={categories.map((category) => ({
+              label: category.name,
+              value: category.id,
+            }))}
           />
         </div>
       </div>
