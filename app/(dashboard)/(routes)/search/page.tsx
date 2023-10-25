3d1f5df657227b4623c7927db93c5bae05595a61
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-10-25 18:24:47 +0700

    Course card

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,13 +1,33 @@
-import { db } from '@/lib/db';
-import { Categories } from './_components/categories';
-import { SearchInput } from '@/components/search-input';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
-const SearchPage = async () => {
+import { db } from '@/lib/db';
+import { getCourses } from '@/actions/get-courses';
+import { SearchInput } from '@/components/search-input';
+import { CoursesList } from '@/components/courses-list';
+import { Categories } from './_components/categories';
+
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+}
+
+const SearchPage: React.FC<SearchPageProps> = async ({ searchParams }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect('/');
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: 'asc',
     },
   });
+
+  const courses = await getCourses({ userId, ...searchParams });
 
   return (
     <>
@@ -15,8 +35,9 @@ const SearchPage = async () => {
         <SearchInput />
       </div>
 
-      <div className='p-6'>
+      <div className='p-6 space-y-4'>
         <Categories items={categories} />
+        <CoursesList items={courses} />
       </div>
     </>
   );
