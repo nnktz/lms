3fbd6cb7b82ff5bb6c8e2150880f7437c1cf9759
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-10-26 17:37:01 +0700

    Teacher analytics dashboard

---
+++ app/(dashboard)/(routes)/teacher/analytics/_components/data-card.tsx
@@ -0,0 +1,28 @@
+import { formatPrice } from '@/lib/format';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
+interface DataCardProps {
+  value: number;
+  label: string;
+  shouldFormat?: boolean;
+}
+
+export const DataCard: React.FC<DataCardProps> = ({
+  value,
+  label,
+  shouldFormat,
+}) => {
+  return (
+    <Card>
+      <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+        <CardTitle className='text-sm font-medium'>{label}</CardTitle>
+      </CardHeader>
+
+      <CardContent>
+        <div className='text-2xl font-bold'>
+          {shouldFormat ? formatPrice(value) : value}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
